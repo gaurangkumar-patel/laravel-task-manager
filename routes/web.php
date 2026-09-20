7a298b2fd5cf1fcef3310131280ad0568e634470
Gaurangkumar Patel <gaurang.technogic@gmail.com>
2026-09-20 19:10:37 +0100

    Add routes/web.php

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::redirect('/', '/tasks');
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::post('/tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
+
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
